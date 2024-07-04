e179bde70edf51967a816dcef2092ad679c26931
Roland <machodin@yandex.ru>
2024-07-05 02:15:41 +0300

    added reg date

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,10 +1,18 @@
 import { ROLES } from '@/auth/enums';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @CreateDateColumn()
+  register_date: Date;
 
   @Column({ default: ROLES.USER })
   auth_level: ROLES;
